d3681c7554b50f8f07522a3c72b2a04945b2fbbf
nb <hello@niklasbuhl.xyz>
2019-05-23 17:38:22 +0200

    run webserver

--- main.py
+++ main.py
@@ -325,6 +325,6 @@ print("Peace out! {0} ms\n\n".format(ms() - now))
 # Web Application
 # ------------------------------------------------------------------------------
 
-# if CONFIG['web']['run']:
-#     if __name__ == "__main__":
-#         app.run(host = '0.0.0.0', port = 80)
+if CONFIG['web']['run']:
+    if __name__ == "__main__":
+        app.run(host = '0.0.0.0', port = 80)
